d42fe45f1ca5fcf6ce53e674c645fee0ef8ed711
azimuth-m <ifandakov@gmail.com>
2024-06-17 23:15:07 +0300

    DiplomnaAccel: 2nd Commit
    - Fixed some logging issues in main
    - Updated config not to use watchodg
    - Updated config to use debug level logging
    
    Signed-off-by: azimuth-m <ifandakov@gmail.com>

--- main/main.cc
+++ main/main.cc
@@ -1,5 +1,3 @@
-#include <iostream>
-#include <bitset>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
@@ -8,8 +6,8 @@
 #include "ADXL345.h"
 
 
-extern "C" void app_main(void)
-{
+extern "C" void app_main(void) {
+    /* Init Section */
     constexpr int MOSI_V = 23;
     constexpr int MISO_V = 19;
     constexpr int SCLK_V = 18;
@@ -20,25 +18,28 @@ extern "C" void app_main(void)
     // SPI1 is internal (not accessible)
     // SPI2 is HSPI
     // SPI3 is VSPI
-    esp_err_t rc = 0;
-    rc = comms.Init(
+
+    esp_err_t rc = ESP_OK;
+    rc |= comms.Init(
             SPI3_HOST,
             MOSI_V,
             MISO_V,
             SCLK_V);
     if (ESP_OK != rc) {
-        // std::cerr << "Return code NOT OK: 0x" << std::hex << rc << std::endl;
+        ESP_LOGD(
+                "main",
+                "In function %s: Failed to initialize spi communications. \
+                    Return code: %d",
+                 __func__, rc); // DEVID CONTENT SHOULD BE 0xE5
     }
 
     comms.RegisterDevice(SPI_MODE, CS_V, 8, 8, 0, 100, 0, 0, 0);
 
-    std::cout << "Return code after register: 0x" << std::hex << rc << std::endl;
+    uint8_t RECEIVED = comms.ReadRegister(REG_DEVID);
+    ESP_LOGD("main", "Reg REG_DEVID: 0x%02X", RECEIVED); // DEVID CONTENT SHOULD BE 0xE5
 
+    /* Main program loop section */
     while (true) {
-        uint8_t RECEIVED = comms.ReadRegister(REG_DEVID);
-        // std::cout << "Reg 0xff: " << std::showbase << std::hex << RECEIVED << std::endl;
-        printf("Reg REG_DEVID: 0x%02X\n", RECEIVED); // DEVID CONTENT SHOULD BE 0xE5
-        // ESP_LOGI();
     }
 
 }
